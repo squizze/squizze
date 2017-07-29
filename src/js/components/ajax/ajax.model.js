a71790525d45e12802984223cbdfa8d06efa7b18
Jota Teles <jose.maciel@vivadecora.com.br>
2017-07-28 21:41:56 -0300

    [#13] - corrige indentacao do AjaxModel

--- src/js/components/ajax/ajax.model.js
+++ src/js/components/ajax/ajax.model.js
@@ -1,35 +1,37 @@
 (function(){
     "use strict";
 
-    angular.module("ajax").factory("Ajax", function($http){
-
-    function get(url, params){
-        if(!params){
-            params = {};
+    function Ajax(){
+        function get(url, params){
+            if(!params){
+                params = {};
+            }
+            var promise = $http({
+                method: "GET",
+                url: url,
+                params: params
+            });
+            return promise;
         }
-        var promise = $http({
-            method: "GET",
-            url: url,
-            params: params
-        });
-        return promise;
-    }
-
-    function post(url, params){
-        if(!params){
-            params = {};
+        function post(url, params){
+            if(!params){
+                params = {};
+            }
+            var promise = $http({
+                method: "POST",
+                url: url,
+                data: params
+            });
+            return promise;
         }
-        var promise = $http({
-            method: "POST",
-            url: url,
-            data: params
-        });
-        return promise;
-    }
 
         return {
             post: post,
             get: get
         };
-    });
+
+    }
+
+    angular.module("ajax").factory("Ajax", Ajax);
+
 })();
