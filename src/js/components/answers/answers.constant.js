c4aeebf1bee04d21b0b86b2a474c1867fefa546c
Jota Teles <josetelesmaciel@gmail.com>
2017-08-01 23:15:38 -0300

    [#11] - Corrige lint de arquivo de constante

--- src/js/components/answers/answers.constant.js
+++ src/js/components/answers/answers.constant.js
@@ -1,27 +1,26 @@
 (function(){
 
-"use strict";
+    "use strict";
 
-angular.module("disc.components.answers").constant("ANSWERS_CONSTANT", AnswersConstant);
+    var AnswersConstant = {
+        "A": {
+            shortAlias: "D",
+            longAlias: "Dominance"
+        },
+        "B": {
+            shortAlias: "I",
+            longAlias: "Influence"
+        },
+        "C": {
+            shortAlias: "S",
+            longAlias: "Steadliness"
+        },
+        "D": {
+            shortAlias: "C",
+            longAlias: "Compliance"
+        }
+    };
 
-var AnswersConstant = {
-"A": {
-shortAlias: "D",
-longAlias: "Dominance"
-},
-"B": {
-shortAlias: "I",
-longAlias: "Influence"
-},
-"C": {
-shortAlias: "S",
-longAlias: "Steadliness"
-},
-"D": {
-shortAlias: "C",
-longAlias: "Compliance"
-}
-};
-
+    angular.module("disc.components.answers").constant("ANSWERS_CONSTANT", AnswersConstant);
 
 })();
