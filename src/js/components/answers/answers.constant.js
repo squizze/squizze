bb0c96e9dfe1400cb9798c022e63b65c8fa33836
Jota Teles <josetelesmaciel@gmail.com>
2017-08-01 23:06:45 -0300

    sem-issue - indenta incorretamente um arquivo para teste

--- src/js/components/answers/answers.constant.js
+++ src/js/components/answers/answers.constant.js
@@ -1,27 +1,27 @@
 (function(){
 
-    "use strict";
+"use strict";
 
-    angular.module("disc.components.answers").constant("ANSWERS_CONSTANT", AnswersConstant);
+angular.module("disc.components.answers").constant("ANSWERS_CONSTANT", AnswersConstant);
 
-    var AnswersConstant = {
-        "A": {
-            shortAlias: "D",
-            longAlias: "Dominance"
-        },
-        "B": {
-            shortAlias: "I",
-            longAlias: "Influence"
-        },
-        "C": {
-            shortAlias: "S",
-            longAlias: "Steadliness"
-        },
-        "D": {
-            shortAlias: "C",
-            longAlias: "Compliance"
-        }
-    };
+var AnswersConstant = {
+"A": {
+shortAlias: "D",
+longAlias: "Dominance"
+},
+"B": {
+shortAlias: "I",
+longAlias: "Influence"
+},
+"C": {
+shortAlias: "S",
+longAlias: "Steadliness"
+},
+"D": {
+shortAlias: "C",
+longAlias: "Compliance"
+}
+};
 
 
 })();
