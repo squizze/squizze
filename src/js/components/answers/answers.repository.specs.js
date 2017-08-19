c61b2fb375202681f988286a393725632820a04f
Jota Teles <jose.maciel@vivadecora.com.br>
2017-08-18 23:11:44 -0300

    [#24] - aplica alteracoes na spec

--- src/js/components/answers/answers.repository.specs.js
+++ src/js/components/answers/answers.repository.specs.js
@@ -1,35 +1,38 @@
 "use strict";
+require("angular-mocks");
 var AnswersRepository = require("./answers.repository");
+var QuestionsAndOptionsMock = require("../../tests/questions-and-options.mock");
 var $cacheFactory;
 var $httpBackend;
-var QuestionsAndOptionsMock;
-var initPromise;
+var QuestionsApi;
 
 describe("[FILMES2017] Testa o AnswersRepository", function(){
 
     beforeEach(function(){
+        angular.mock.module("ngMockE2E");
         angular.mock.module("disc.components.answers");
         angular.mock.module("disc.mocks");
     });
 
     beforeEach( inject( function($injector) {
         AnswersRepository = $injector.get("AnswersRepository");
-        $httpBackend = $injector.get("$httpBackend");
         $cacheFactory = $injector.get("$cacheFactory");
         QuestionsAndOptionsMock = $injector.get("QuestionsAndOptionsMock");
+        QuestionsApi = $injector.get("QuestionsApi");
         QuestionsAndOptionsMock.setupMocks();
-        initPromise = AnswersRepository.init();
+        $httpBackend = $injector.get("$httpBackend");
     }));
 
     it("[GUARDIANS_OF_GALAXY_2] existe o AnswersRepository", function(){
         expect(AnswersRepository).toBeDefined();
         expect($cacheFactory).toBeDefined();
+        expect(QuestionsApi).toBeDefined();
     });
 
-
-    it("[LOGAN] existe o init", function(){
-        initPromise.then(function(results){
-            console.log("funcionei", results);
+    it("[LOGAN]", function(){
+        var promise = AnswersRepository.init();
+        promise.then(function(){
+            console.log("oi");
         });
     });
 
