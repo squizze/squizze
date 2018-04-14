deb45c6b947f1f3272d8460d618149f3b40e536e
Jota Teles <josetelesmaciel@gmail.com>
2018-04-14 11:15:48 -0300

    NO-ISSUE - apply small change to Home to test deploy

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,9 +2,9 @@
 
   <div>
     <h1> Descubra seu perfil DISC</h1>
-    <h2> Com algumas questões simples descobriremos seu perfil DISC!</h2>
+    <h2> Com algumas questões simples descobriremos seu perfil DISC.</h2>
 
-    <router-link :to="{name: 'question', params: {question_id: 1}}">Começar!</router-link>
+    <router-link :to="{name: 'question', params: {question_id: 1}}">Começar agora!</router-link>
 
   </div>
 
